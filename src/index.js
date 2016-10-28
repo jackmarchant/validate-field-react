e338a87647668e78a7c61c4fda1a593efb673499
Jack Marchant <jack@expert360.com>
2016-10-28 15:40:43 +1100

    Updated API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import ValidateField from './ValidateField';
-import ValidateForm from './ValidateForm';
+import Field from './ValidateField';
+import Form from './ValidateForm';
+
+export const ValidateField = Field;
+export const ValidateForm = Form;
 
 export default {
   ValidateField,
   ValidateForm
-}
+};
